8fd56f372e3b4a8555579d2644fa2ef266bd813e
Aladar <aladarmiao1@gmail.com>
2023-05-23 12:52:48 -0700

    update PG port; add NEXT command

--- RGBIntegrationScript_copy.cpp
+++ RGBIntegrationScript_copy.cpp
@@ -8,7 +8,7 @@
 
 #define SERVER_IP "192.168.10.1"  // Replace with the actual server IP address
 // 192.168.10.1
-#define SERVER_PORT 21   // Replace with the actual server port try 20 or 88
+#define SERVER_PORT 9999 // reference the manual 服务端监听端口固定为9999
 
 int main() {
     WSADATA wsaData;
@@ -53,21 +53,21 @@ int main() {
     }
 
     // Construct the IC command
-    unsigned char data[] = { 0x02, 'I', 'C', 'C', 0, 0, 0x03 };
-    int dataSize = sizeof(data);
+    unsigned char IC_data[] = { 0x02, 'I', 'C', 'C', 0, 0, 0x03 };
+    int IC_dataSize = sizeof(IC_data);
 
     // Send the IC command to the server
-    if (send(sockfd, (char*)data, dataSize, 0) == SOCKET_ERROR) {
+    if (send(sockfd, (char*)IC_data, IC_dataSize, 0) == SOCKET_ERROR) {
         std::cerr << "Failed to send the IC command." << std::endl;
         closesocket(sockfd);
         WSACleanup();
         return 1;
     }
 
-    // Receive the response from the server
-    char buffer[1024];
-    memset(buffer, 0, sizeof(buffer));
-    int bytesReceived = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
+    // Receive the IC response from the server
+    char IC_buffer[1024];
+    memset(IC_buffer, 0, sizeof(IC_buffer));
+    int bytesReceived = recv(sockfd, IC_buffer, sizeof(IC_buffer) - 1, 0);
     if (bytesReceived == SOCKET_ERROR) {
         std::cerr << "Failed to receive response from the server." << std::endl;
         closesocket(sockfd);
@@ -75,28 +75,28 @@ int main() {
         return 1;
     }
 
-    // Process the response
-    std::string response(buffer, bytesReceived);
+    // Process the IC response
+    std::string IC_response(IC_buffer, bytesReceived);
 
     // Check if the response has all the fields
-    if (response.length() < 7) {
+    if (IC_response.length() < 7) {
         std::cerr << "Invalid response from the server. Missing fields." << std::endl;
         closesocket(sockfd);
         WSACleanup();
         return 1;
     }
 
-    // Extract the fields from the response
-    std::cout << "Response: " << response << std::endl;
+    // Extract the fields from the IC response
+    std::cout << "Response: " << IC_response << std::endl;
 
-    unsigned char stx = response[0];
-    std::string command = response.substr(1, 2);
-    std::string cmdType = response.substr(3, 1);
-    std::string acknowledge = response.substr(4, 1);
-    std::string reserve = response.substr(5, 1);
-    unsigned char etx = response[6];
+    unsigned char stx = IC_response[0];
+    std::string command = IC_response.substr(1, 2);
+    std::string cmdType = IC_response.substr(3, 1);
+    std::string acknowledge = IC_response.substr(4, 1);
+    std::string reserve = IC_response.substr(5, 1);
+    unsigned char etx = IC_response[6];
 
-    // Check if the response has the correct format
+    // Check if the IC response has the correct format
     if (stx != 0x02 || command != "IC" || etx != 0x03) {
         std::cerr << "Invalid response from the server. Incorrect format." << std::endl;
         closesocket(sockfd);
@@ -104,13 +104,37 @@ int main() {
         return 1;
     }
 
-    // Check if the connection was successful (Acknowledge = 0)
+    // Check if the IC was successful (Acknowledge = 0)
     if (acknowledge != "0") {
         std::cerr << "NG response from the server. Invalid Acknowledge value. Connection is not established." << std::endl;
         closesocket(sockfd);
         WSACleanup();
         return 1;
     }
+
+    // Construct the NEXT command
+    char NEXT_data[] = { 0x02, 'N', 'E', 'X', 'T', 'C', 0, 0, 1, 1, 1, 0, 0x03 };
+    int NEXT_dataSize = sizeof(NEXT_data);
+
+    // Send the IC command to the server
+    if (send(sockfd, (char*)IC_data, IC_dataSize, 0) == SOCKET_ERROR) {
+        std::cerr << "Failed to send the IC command." << std::endl;
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
+    }
+
+    // Receive the IC response from the server
+    char buffer[1024];
+    memset(IC_buffer, 0, sizeof(IC_buffer));
+    int bytesReceived = recv(sockfd, IC_buffer, sizeof(IC_buffer) - 1, 0);
+    if (bytesReceived == SOCKET_ERROR) {
+        std::cerr << "Failed to receive response from the server." << std::endl;
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
+    }
+
 
     // Close the socket and clean up Winsock
     closesocket(sockfd);
